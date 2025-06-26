66faaf73fde38e6dce661268e48c492bf9a061dc
AiT7 <STTEMA77@GMAIL.COM>
2025-06-26 16:47:10 +0300

    Создание нового проекта
    Попробуй создать клон VS или Brotato

---
+++ Assets/VLIkeProto/Scripts/Manager/Bootstrap/GameBootstrap.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace VLIkeProto.Scripts.Manager.Bootstrap
+{
+    public class GameBootstrap : MonoBehaviour
+    {
+       //TODO
+       //1)GameManager 
+        //StateMachete for gameState (gameplay and shop)
+       //2)ObjectPool (generic)
+        //EnemyPool, ExpObj, ProjectileObj
+       //3)Fabric for Instance GO
+        //Enemy, Projectiles=>Vfx, Exp, PickUp, Player 
+        
+        
+        
+        
+    }
+}
